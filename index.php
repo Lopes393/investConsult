0fd0b4e473f9d72ccf94a661bb98818276c36239
Murilo Lopes <mloopes11@gmail.com>
2020-12-21 11:43:38 -0300

    Version 0.5

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Invest Consult</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"> 
     </head>
     <body>
         <?php
