2185082437539dc24d16014d200fc2d193c2fddb
Murilo Lopes <mloopes11@gmail.com>
2020-12-21 11:41:43 -0300

    Version 0.3

--- apiConsult.php
+++ apiConsult.php
@@ -1,6 +1,4 @@
 <?php
-define("API","https://mdgateway.easynvest.com.br/iwg/snapshot/?t=webgateway&c=");
-define("TOKEN","a1733d6e2f9326e7329a688245237d91");
 $url = 'https://mdgateway.easynvest.com.br/iwg/snapshot/?t=webgateway&c=8841484&q=PETR4,29,0,10|IBOV,1,0,10|VALE3,1,0,10';
 //$url = API . TOKEN . '&q=' . ACTIONS;
 
--- index.php
+++ index.php
@@ -6,19 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Invest Consult</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <link rel="stylesheet" href="assets/css/color.css">
-        <link rel="stylesheet" href="assets/css/telas.css">
-        
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"> 
     </head>
     <body>
         <?php
             require 'apiConsult.php';
-            // print '<h1 class="title">Servidor de deploy</h1>';
-            // print '<div class="flex">';
-            //     require 'layout.php';
-            // print '</div>';
         ?>
     </body>
 </html>
